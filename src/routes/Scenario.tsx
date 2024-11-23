97b6b0839c13d9460fe9eb6b5a024323503e428c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-23 16:44:14 -0600

    updated scenario descriptions

--- src/routes/Scenario.tsx
+++ src/routes/Scenario.tsx
@@ -2,11 +2,18 @@ import { useLocation } from "react-router-dom";
 
 const Scenario = () => {
   const location = useLocation();
-  const data = location.state;
+  const index = location.state;
+
+  const descriptions = [
+    "Your friend is a lonely college student isolated by your peers; it's taking a toll on their mental health.",
+    "Your friend is a college student frequently bullied by your peers.",
+    "Your friend is a stressed college student constantly worrying about their grades.",
+  ];
+  const scenario = `${descriptions[index]} One day, you decide to help out your friend by offering some advice. What will you tell them?`;
 
   return (
     <>
-      <h1>{data}</h1>
+      <p>{scenario}</p>
     </>
   );
 };
