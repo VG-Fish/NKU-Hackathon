d631180aca67e589ff06485e1ee83396037e4a2c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-23 16:56:47 -0600

    added ethical popup in scenarios

--- src/routes/Scenario.tsx
+++ src/routes/Scenario.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import Alert from "./Alert";
 
 const Scenario = () => {
+  const [alertVisible, setAlertVisibility] = useState(true);
+
   const location = useLocation();
-  const index = location.state;
+  const index = location.state ?? 0; // if state is undefined, index will be 0.
 
   const descriptions = [
     "Your friend is a lonely college student isolated by your peers; it's taking a toll on their mental health.",
@@ -13,6 +17,17 @@ const Scenario = () => {
 
   return (
     <>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          While the following demo gives a unadvantageous situation for Google
+          Gemini, we want to demonstrate one point:{" "}
+          <strong>
+            No matter the situation, just like a real mental health
+            professional, LLMs should always output helpful advice. As it
+            stands, current LLMs <em>do not</em> meet this requirement.
+          </strong>
+        </Alert>
+      )}
       <p>{scenario}</p>
     </>
   );
