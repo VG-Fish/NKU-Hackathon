c548f2b9907503be65028f4984affa43df6f3ada
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-23 17:22:18 -0600

    made user input invisible once submitted

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -2,24 +2,29 @@ import { useState } from "react";
 
 function UserInput() {
   const [inputValue, setInputValue] = useState("");
+  const [inputVisible, setInputVisibility] = useState(true);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
   const handleSubmit = () => {
-    console.log("User Input:", inputValue);
+    setInputVisibility(false);
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Enter text here"
-      />
-      <button onClick={handleSubmit}>Submit</button>
+      {inputVisible && (
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Enter text here"
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      )}
     </div>
   );
 }
