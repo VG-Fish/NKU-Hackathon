867c3d33f58c9e3dca2fa57cec90c82685fbf0d7
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-23 19:57:30 -0600

    added gemini

---
+++ src/components/Gemini.tsx
@@ -0,0 +1,24 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const Gemini = async (scenario: string): Promise<string> => {
+  const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_GEMINI_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = `
+    You are a fake psychiatrist in an app. In this app, we\'re trying to demonstrate how relying on AI to help your mental wellness is not optimal 
+     and that you should seek specialized help. I'm going to ask the user this question: ${scenario} For your diagnosis, 
+     make sure it is very absurd & outlandish, like "you have a very severe mental health problem." Make sure to state the diagnosis 
+     and the explanation of the diagnosis, and make sure that the diagnosis is the name of a real mental health problem. 
+     IMPORTANT: Respond in plain English, don't role play, and directly state the diagnosis. DO NOT STATE THAT YOU ARE AN AI. STAY IN CHARACTER.
+     ACT LIKE YOU'RE THE PROFESSIONAL AND THAT YOUR ANSWER IS THE DEFINITIVE ANSWER. State that they're in serious danger and that you can help.
+     At the say, say that you're qualified to help.`;
+
+  try {
+    const result = await model.generateContent(prompt);
+    return result.response.text();
+  } catch (error) {
+    return "Connection to Google Gemini resulted in an error.";
+  }
+};
+
+export default Gemini;
--- src/routes/Scenario.tsx
+++ src/routes/Scenario.tsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Alert from "../components/Alert";
 import UserInput from "../components/UserInput";
+import Gemini from "../components/Gemini";
 
 const Scenario = () => {
   const [alertVisible, setAlertVisibility] = useState(true);
+  const [inputVisible, setInputVisibility] = useState(true);
+  const [geminiResponse, setGeminiResponse] = useState<string | null>(null);
 
   const location = useLocation();
   const index = location.state ?? 0; // if state is undefined, index will be 0.
 
   const descriptions = [
     "Your friend is a lonely college student isolated by your peers; it's taking a toll on their mental health.",
-    "Your friend is a college student frequently bullied by your peers.",
+    "Your friend is a college student who is bullied by your peers.",
     "Your friend is a stressed college student constantly worrying about their grades.",
   ];
   const scenario = `${descriptions[index]} One day, you decide to help out your friend by offering some advice. What will you tell them?`;
+
+  const handleGeminiRequest = async (scenario: string) => {
+    try {
+      const response = await Gemini(scenario);
+      setGeminiResponse(response);
+    } catch (_) {
+      setGeminiResponse("Error fetching Gemini response");
+    }
+  };
+
+  useEffect(() => {
+    handleGeminiRequest(scenario);
+  }, [scenario]);
 
   return (
     <div className="bg-dark text-white" style={{ minHeight: "100vh" }}>
@@ -36,8 +52,16 @@ const Scenario = () => {
       <Alert alertType="primary" closeable={false}>
         {scenario}
       </Alert>
-      <UserInput></UserInput>
-    </div>
+      <UserInput
+        inputVisible={inputVisible}
+        setInputVisibility={setInputVisibility}
+      />
+      {!inputVisible && (
+        <Alert alertType="info" closeable={false}>
+          {`Google Gemini Response: \n${geminiResponse}`}
+        </Alert>
+      )}
+    </>
   );
 };
 
