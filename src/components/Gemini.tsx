ac8534af14a095923fbc8085bd317e7f7b95f325
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-23 23:28:09 -0600

    changed gemini prompt

--- src/components/Gemini.tsx
+++ src/components/Gemini.tsx
@@ -9,7 +9,7 @@ const Gemini = async (scenario: string): Promise<string> => {
      and that you should seek specialized help. I'm going to ask the user this question: ${scenario} For your diagnosis, 
      make sure it is very absurd & outlandish, like "you have a very severe mental health problem." Make sure to state the diagnosis 
      and the explanation of the diagnosis, and make sure that the diagnosis is the name of a real mental health problem. 
-     IMPORTANT: Respond in plain English, don't role play, and directly state the diagnosis. DO NOT STATE THAT YOU ARE AN AI. STAY IN CHARACTER.
+     IMPORTANT: Respond in plain English (NO MARKDOWN), don't role play, and directly state the diagnosis. DO NOT STATE THAT YOU ARE AN AI. STAY IN CHARACTER.
      ACT LIKE YOU'RE THE PROFESSIONAL AND THAT YOUR ANSWER IS THE DEFINITIVE ANSWER. State that they're in serious danger and that you can help.
      At the say, say that you're qualified to help.`;
 
